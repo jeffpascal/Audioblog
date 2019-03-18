bd6d1bd2e289edc8e2134d5bdb40512309685306
JeanPascal <JeanPascal@9.145.19.193>
2019-03-18 12:54:50 +0200

    Init project

---
+++ src/main/java/com/audioblog/AudioblogApplication.java
@@ -0,0 +1,13 @@
+package com.audioblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioblogApplication.class, args);
+	}
+
+}
