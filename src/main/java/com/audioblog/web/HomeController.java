42eb4f3dd1de3dda0474313a0536b2b02fe8e677
JeanPascal <JeanPascal@DESKTOP-N8R826T.aag-de.ibmmobiledemo.com>
2019-03-22 11:27:05 +0200

    Added swagger and refactored packages

--- src/main/java/com/audioblog/web/HomeController.java
+++ src/main/java/com/audioblog/web/HomeController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.audioblog.PostComponent;
-import com.audioblog.WallComponent;
 
 //Because HomeController is annotated with @Controller, 
 //Spring’s component scanning automatically discovers it and creates an 
@@ -20,14 +18,14 @@ public class HomeController {
 	
 	//HTTP GET request is received for the root path /, then this method handles that request
 	@GetMapping(path = "/")
-	public WallComponent home(){
+	public com.audioblog.user.WallComponent home(){
 		
-		PostComponent post = new PostComponent("Azi", "Status", new Date(12,11,13), null);
-		PostComponent post2 = new PostComponent("Azi ploua afara", "Status afara", new Date(12,11,13), null);
-		PostComponent post3 = new PostComponent("Azi ploua afara", "Status afara", new Date(12,11,13), null);
-		PostComponent post4 = new PostComponent("Azi ploua afara", "Status afara", new Date(12,11,13), null);
+		com.audioblog.user.PostComponent post = new com.audioblog.user.PostComponent("Azi", "Status", new Date(12,11,13), null);
+		com.audioblog.user.PostComponent post2 = new com.audioblog.user.PostComponent("Azi ploua afara", "Status afara", new Date(12,11,13), null);
+		com.audioblog.user.PostComponent post3 = new com.audioblog.user.PostComponent("Azi ploua afara", "Status afara", new Date(12,11,13), null);
+		com.audioblog.user.PostComponent post4 = new com.audioblog.user.PostComponent("Azi ploua afara", "Status afara", new Date(12,11,13), null);
 		
-		WallComponent wall = new WallComponent(new LinkedList<>());
+		com.audioblog.user.WallComponent wall = new com.audioblog.user.WallComponent(new LinkedList<>());
 		wall.addPost(post);
 		wall.addPost(post2);
 		wall.addPost(post3);
